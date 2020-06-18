01d228844c363c87b18685735a03f1a73844c1d9
tmodugula <tmodugula@IN-PF1CUBJN>
2020-06-18 13:27:05 +0530

    add enacpsulation 1st tym from eclipse

---
+++ Encapsulation/src/Encaps/MainMethod.java
@@ -0,0 +1,19 @@
+package Encaps;
+
+public class MainMethod 
+{
+	public static void main(String[] args)
+	{
+ Student s1=new Student();
+ s1.setcity("vijayawada");
+ s1.setempId("123we");
+ s1.setName("Teja");
+ s1.getcity();
+ s1.getName();
+ s1.getempId();
+ System.out.println(s1.getcity());
+ System.out.println(s1.getName());
+ System.out.println(s1.getempId());
+	}
+
+}
---
+++ Encapsulation/src/Encaps/Student.java
@@ -0,0 +1,30 @@
+package Encaps;
+
+public class Student 
+{
+  String name,empId,city;
+  public String getName()
+  {
+	  return name;
+  }
+  public void setName(String Name)
+  {
+	  this.name=name;
+  }
+  public String getempId()
+  {
+	  return empId;
+  }
+  public void setempId(String empId)
+  {
+	  this.empId=empId;
+  }
+  public String getcity()
+  {
+	  return city;
+  }
+  public void setcity(String city)
+  {
+	  this.city=city;
+  }
+}
